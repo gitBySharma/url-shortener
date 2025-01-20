60c7d53266c011a3f7daac824e3b0322394f05bf
gitBySharma <fetchsubhankar@gmail.com>
2025-01-20 19:58:28 +0530

    minor code improvement

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const express = require("express");
-const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
@@ -21,7 +20,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.set("trust proxy", 1); //trust the first proxy (e.g when using Nginx)
 
 
